a1b2d86f77cb33c8745dd20426cd01bb197f2409
unknown <1345036885@qq.com>
2019-05-13 18:03:37 +0900

    update

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,21 +1,21 @@
-let fetch  = require("node-fetch");
+let fetch = require("node-fetch");
 
-export const  getApi = (url,query,variables=null)=>{
-  return new Promise(( resolve ,reject) => {
-      fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-        },
-        body: JSON.stringify({
-          query,
-          variables
-        })
+export const getApi: any = (url: string, query: string, variables: any = null) => {
+  return new Promise((resolve: any, reject: any) => {
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({
+        query,
+        variables
       })
-      .then(r => r.json())
-      .then(res => {
-        if(res.errors){reject(res.errors);return}
+    })
+      .then((r: any) => r.json())
+      .then((res: { errors: any, data: any }) => {
+        if (res.errors) { reject(res.errors); return }
         resolve(res.data)
       });
   })
